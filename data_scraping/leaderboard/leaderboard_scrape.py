7ae3006923e58f79cf66a1c65d7f0df089e9a4ff
hassannaveed1997 <hassannaveed@live.com>
2024-03-01 21:30:38 -0800

    bug fixes

--- data_scraping/leaderboard/leaderboard_scrape.py
+++ data_scraping/leaderboard/leaderboard_scrape.py
@@ -4,12 +4,12 @@ import json
 import sys
 
 with open("../../Data/assets/year_to_column_mapping.json", "r") as f:
-    year_to_column_mapping = json.loads(f.read())
+    year_to_col_name_mapping = json.loads(f.read())
 
 with open("../../Data/assets/division_mapping.json", "r") as f:
     division_mapping = json.loads(f.read())
 
-def scrape_data_for_year(year, division = "mens"):
+def scrape_data_for_year(year, division = "mens",num_pages = None):
     athlete_info_data = []
     scores_data = []
 
@@ -19,10 +19,11 @@ def scrape_data_for_year(year, division = "mens"):
     url = f'https://c3po.crossfit.com/api/leaderboards/v2/competitions/open/{year}/leaderboards?view=0&division={division_num}&region=0&scaled=0&sort=0&page=1'
     response = urlopen(url)
     data = json.loads(response.read())
-    total_pages = data['pagination']['totalPages']
+    if num_pages is None:
+        num_pages = data['pagination']['totalPages']
     num_workouts = len(data['leaderboardRows'][0]['scores'])
 
-    for i in range(1,total_pages+1):
+    for i in range(1,num_pages+1):
         url = f'https://c3po.crossfit.com/api/leaderboards/v2/competitions/open/{year}/leaderboards?view=0&division={division_num}&region=0&scaled=0&sort=0&page={i}'
         response = urlopen(url)
         api_response = json.loads(response.read())
@@ -42,16 +43,16 @@ def scrape_data_for_year(year, division = "mens"):
             
         # print every 10th page to check progress
         if i % 10 == 0:
-            print(f"Finished page {i} of {total_pages}")
+            print(f"Finished page {i} of {num_pages}")
 
     # Create DataFrames
     athlete_info_df = pd.DataFrame(athlete_info_data, columns=["id", "name", "age", "height", "weight"])
-    scores_df = pd.DataFrame(scores_data, columns=["id"]+year_to_col_name_mapping[year])
+    scores_df = pd.DataFrame(scores_data, columns=["id"]+year_to_col_name_mapping[str(year)])
 
 
 
     # save to csv
-    athlete_info_df.to_csv(f'{year}_{division}_info.csv')
-    scores_df.to_csv(f'{year}_{division}_scores.csv')
+    athlete_info_df.to_csv(f'../../Data/{year}_{division}_info.csv')
+    scores_df.to_csv(f'../../Data/{year}_{division}_scores.csv')
 
     return athlete_info_df, scores_df
