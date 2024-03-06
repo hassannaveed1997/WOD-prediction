f9f6176592b26e7ec20bf69087fd054d2025de08
Hassan Naveed <hassannaveed@live.com>
2024-03-05 17:56:53 -0800

    feat/preprocessing (#3)
    * added conversion of units function
    
    * added processing of scaled workouts

--- modeling/preprocessing.py
+++ modeling/preprocessing.py
@@ -1,6 +1,104 @@
 import pandas as pd
 import numpy as np
 
+LB_MULTIPLIER = 2.20462
+CM_MULTIPLIER = 0.393701
+
+def convert_units(df, type, columns = None):
+    """
+    Helper function to convert units from metric to imperial
+
+    Parameters:
+    ----------
+    df: pd.DataFrame
+        The dataframe with the columns to be converted
+    type: str
+        The type of conversion. It can be either 'weight' or 'height'
+    columns: list
+        The columns to be converted. If None, all columns would be tested except 'name'
+    
+    Returns:
+    -------
+    df: pd.DataFrame
+        The modified dataframe with the converted columns
+
+    """
+    # if no columns are provided, use all columns except 'name'
+    if not columns:
+        columns = list(df.columns)
+        if 'name' in columns:
+            columns.remove('name')
+
+    # determine the multiplier and key words to remove based on the type
+    if type == 'weight':
+        multiplier = LB_MULTIPLIER
+        key_words_to_remove = ['kg', 'lb']
+
+    elif type == 'height':
+        multiplier = CM_MULTIPLIER
+        key_words_to_remove = ['cm', 'in']
+    else:
+        raise ValueError('type must be "weight" or "height"')
+
+    # iterate through each column and convert the units
+    for col in columns:
+        if df[col].dtype == 'object': # if its not an object, we can skip
+            # determine which rows contain the key words
+            rows_with_key_words = df[col].str.contains(key_words_to_remove[0], na = False) 
+            if not rows_with_key_words.any():
+                continue
+            for word in key_words_to_remove:
+                df[col] = df[col].str.replace(word, '', regex = False)
+            df[col] = df[col].str.strip()
+
+
+            df[col] = df[col].astype(float)
+            df[col] = df[col] * multiplier
+            print(f"Converted {col} to {type} in imperial units")
+    return df
+
+
+def seperate_scaled_workouts(df, columns = None):
+    """
+    Seperates the scaled and foundation workouts into different columns, to be treated as different workouts
+
+    Parameters
+    ----------
+    df : pd.DataFrame
+        The dataframe containing the workouts
+    columns : list
+        The columns to be treated as workouts. If None, the function will look for columns with a "." in the name
+    
+    Returns
+    -------
+    pd.DataFrame
+        The dataframe with additional columns for the scaled and foundation workouts
+    """
+    df.copy()
+    if columns is None:
+        columns = [col for col in df.columns if "." in col] # generally open workouts have a "." in the name, e.g 17.4
+    mapping = {" - f": "foundation", " - s": "scaled"}
+    for col in columns:
+        if df[col].dtype != "object": # if the column is not a string,
+            continue
+
+        df[col] = df[col].str.replace("lbs", "") # in case there are lbs in the column
+        df[col] = df[col].str.replace("reps", "").str.strip() # if there were reps in column
+
+        for key, value in mapping.items():
+            rows_that_contain_key = df[col].str.contains(key)
+
+            # keep row as it is for those containing the key, but NA for others
+            if rows_that_contain_key.any():
+                new_col = f'{col}_{value}'
+                df[new_col] = df[col].where(rows_that_contain_key)
+                df[new_col] = df[new_col].str.replace(key, "")
+
+            # remove rows from original column
+            df.loc[rows_that_contain_key==1, col] = np.nan
+                    
+    return df
+
 def convert_to_datetime(dt_col):
     """
     Converts a whole column to datetime if it is of type object.
