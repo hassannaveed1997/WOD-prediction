06ec519e0a33c65354adbec7294d44157ff091a2
hassannaveed1997 <hassannaveed@live.com>
2024-06-19 11:48:23 -0700

    changed name

--- setup.py
+++ setup.py
@@ -6,13 +6,13 @@ from setuptools import setup, find_packages
 with open("requirements.txt") as f:
     REQUIREMENTS = f.read().splitlines()
 
-version_file_path = os.path.join(os.path.dirname(__file__), "pipeline_objects/VERSION")
+version_file_path = os.path.join(os.path.dirname(__file__), "wod_predictor/VERSION")
 
 with open(version_file_path) as f:
     VERSION = f.read().strip()
 
 setup(
-    name="pipeline_objects",
+    name="wod_predictor",
     version=VERSION,
     description="Library for predicting Workout of the Day (WOD) scores for CrossFit athletes",
     url="https://github.com/hassannaveed1997/WOD-prediction",
