0ab120808acde757609fc797f659fc844ea7b441
hassannaveed1997 <hassannaveed@live.com>
2024-10-08 19:04:58 -0700

    added class docstring and formatting

--- wod_predictor/feature_engineering_parts/misc.py
+++ wod_predictor/feature_engineering_parts/misc.py
@@ -1,11 +1,20 @@
 class DropFeatures:
+    """
+    Identifies and drops features with more than threshold percentage of missing values
+
+    Parameters:
+    ----------
+    threshold: float
+        threshold percentage of missing values to make feature "dropable"
+    """
+
     def __init__(self, threshold=0.9):
         self.threshold = threshold
         self.features_to_drop = []
 
     def fit(self, df):
         """
-        identifies features with more than threshold percentage of missing values 
+        identifies features with more than threshold percentage of missing values
         """
         self.features_to_drop = df.columns[df.isnull().mean() > self.threshold]
 
@@ -15,7 +24,7 @@ class DropFeatures:
         """
         safe_features_to_drop = set(self.features_to_drop).intersection(df.columns)
         return df.drop(columns=safe_features_to_drop)
-    
+
     def fit_transform(self, df, threshold=0.9):
         self.fit(df, threshold)
         return self.transform(df)
